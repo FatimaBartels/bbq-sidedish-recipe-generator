627c15503a4d21cc8955cf83c6671e6404901870
FatimaBartels <bartels.fatima@gmail.com>
2024-06-21 12:12:36 +0200

    Added the AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
+function displayRecipe(response) {
+  console.log("recipe generated");
+  new Typewriter("#recipe", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 0,
+    cursor: null,
+  });
+}
+
 function generateRecipe(event) {
   event.preventDefault();
 
-  new Typewriter("#recipe", {
-    strings: "Macaroni Salad",
-    autoStart: true,
-    delay: 1,
-    cursor: null,
-  });
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "f80c43e79144055afb51f7885ft48o7e";
+  let prompt = `User instructions: Generate a barbecue side dish recipe about ${instructionsInput.value}`;
+  let context =
+    "You are a very good cook and love to make the best and simple side dishes. Your mission is to generate a simple and easy to make side dish recipe for barbecue with photo in HTML format. Make sure to follow the user instructions.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating recipe");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#recipe-generator-form");
